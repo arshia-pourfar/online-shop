f246ae85cfa4b203e30a8baf24fc9d35fd7f3d51
arshiapourfar <arshiapourfar@gmail.com>
2025-08-09 23:50:38 +0330

    create render db

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ import statusRoutes from './routes/statuses';
 import reportsRouter from './routes/reports';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());

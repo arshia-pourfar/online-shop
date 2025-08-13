067e54ea2790a770290c4f1c3bae09cefd1329a6
arshiapourfar <arshiapourfar@gmail.com>
2025-08-13 19:58:55 +0330

    fix: run prisma generate after install

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // هر ایمپورت از server رو نادیده بگیر
+    config.resolve.alias['server'] = false;
+    return config;
+  },
 };
 
 export default nextConfig;

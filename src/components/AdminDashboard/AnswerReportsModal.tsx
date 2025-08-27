1a344fd8f089820a2ff3923407e6294f7bdf826c
arshiapourfar <arshiapourfar@gmail.com>
2025-08-27 15:39:06 +0330

    add light mode and add paigination in shop page

--- src/components/AdminDashboard/AnswerReportsModal.tsx
+++ src/components/AdminDashboard/AnswerReportsModal.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReply } from "@fortawesome/free-solid-svg-icons";
-import { Report } from "../types/report";
+import { Report } from "../../types/report";
 
 interface AnswerReportsModalProps {
     show: boolean;
